6d375742a7335020e50fc24c3d6876a0b836dc1c
Hades.cool <723760950@qq.com>
2017-04-05 18:21:28 +0800

    v1.0

---
+++ utilspackager/src/main/java/com/hades/utilspackager/data/DbController.java
@@ -0,0 +1,45 @@
+package com.hades.utilspackager.data;
+
+import android.content.Context;
+
+import com.litesuits.orm.LiteOrm;
+
+
+/**
+ * Created by Hades on 2016/11/25.
+ * 全局单例，因此在切换平台时需要释放数据库调用releaseReference()
+ * 退出登录，删除对应表或数据库
+ */
+public class DbController {
+
+    public static DbController getInstance(){
+        return SingleInstace.instance;
+    }
+
+
+    private LiteOrm mLiteOrm;
+    public void initDb(Context context, String dbname){
+        if (mLiteOrm == null){
+            mLiteOrm = LiteOrm.newSingleInstance(context, dbname);
+            mLiteOrm.setDebugged(true);
+        }
+    }
+
+    public LiteOrm getmLiteOrm() {
+        return mLiteOrm;
+    }
+
+    public void closeDb(){
+        if (null != mLiteOrm){
+            mLiteOrm.close();
+            mLiteOrm = null;
+        }
+    }
+
+    private static class SingleInstace{
+        public static DbController instance = new DbController();
+    }
+
+    private DbController(){}
+
+}
